fe68c52b3dc9d3b34ad65e021e036d696017614f
AlexandraMarkova <a.markova@ukr.net>
2021-02-09 16:54:03 +0200

    делает рефакторинг и задействует селектор, также запуск таймера переносит в конструктор

--- timer.js
+++ timer.js
@@ -1,14 +1,14 @@
-const refs = {
-    days: document.querySelector('span[data-value="days"]'),
-    hours: document.querySelector('span[data-value="hours"]'),
-    mins: document.querySelector('span[data-value="mins"]'),
-    secs: document.querySelector('span[data-value="secs"]'),
-}
-
+ 
 class CountdownTimer {
     constructor (selector, targetDate) {
-        this.selector = selector;
         this.targetDate = targetDate;
+        this.refs = {
+          days: document.querySelector(`${selector} [data-value="days"]`),
+          hours: document.querySelector(`${selector} [data-value="hours"]`),
+          mins: document.querySelector(`${selector} [data-value="mins"]`),
+          secs: document.querySelector(`${selector} [data-value="secs"]`),     
+      };
+        this.start();    
     }
 
     start() { 
@@ -21,29 +21,29 @@ class CountdownTimer {
             // console.log(time);
 
             this.updateClockFace(time);
-   }, 1000);
+        }, 1000);
     } 
+     
 
-    updateClockFace(time) { 
+    updateClockFace(time) {
         const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
         const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
         const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
         const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
         // console.log(`${days}:${hours}:${mins}:${secs}`);
-        refs.days.textContent = `${days}`;
-        refs.hours.textContent = `${hours}`;
-        refs.mins.textContent = `${mins}`;
-        refs.secs.textContent = `${secs}`;
+   
+          this.refs.days.textContent = `${days}`;
+          this.refs.hours.textContent = `${hours}`;
+          this.refs.mins.textContent = `${mins}`;
+          this.refs.secs.textContent = `${secs}`;     
     }
 
     pad(value) { 
         return String(value).padStart(2, '0');
-    }
+    } 
+    
 };
 
 const timer = new CountdownTimer('#timer-1', new Date('Jun 20, 2021'));
-
-
-
-timer.start();
+// const timer2 = new CountdownTimer('#timer-2', new Date('Jun 20, 2022'));
